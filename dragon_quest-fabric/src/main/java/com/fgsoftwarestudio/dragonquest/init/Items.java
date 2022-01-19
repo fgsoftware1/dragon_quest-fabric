83d917c9c64b1024068c0938854df1091425c2fc
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-01-19 20:49:49 +0000

    commit
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

--- dragon_quest-fabric/src/main/java/com/fgsoftwarestudio/dragonquest/init/Items.java
+++
@@ -1,8 +0,0 @@
-package com.fgsoftwarestudio.dragonquest.init;
-
-import com.fgsoftwarestudio.dragonquest.Reference;
-import net.minecraft.item.Item;
-
-public class Items {
-    public static Item TEST_ITEM = new Item(new Item.Settings().group(Reference.ITEM_GROUP));
-}
